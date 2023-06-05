674d75756d967e05c57fab2136898dc3d7bb09ca
SonDaAe <103577644+SonDaAe@users.noreply.github.com>
2023-06-05 11:18:36 +0900

    재고 수정 팝업

--- app/src/main/java/kr/ac/kumoh/s20190610/first/EditActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190610/first/EditActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -21,6 +22,7 @@ class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_edit)
 
         edit = findViewById(R.id.edit_btn)
